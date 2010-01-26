99cf95401f486975c7546ca999147d0b05f68ea9
vpradeep <devnull@localhost>
2010-01-26 22:34:03 +0000

    Now using /tmp to store calibration temporary files

--- pr2_calibration_launch/write_urdf/write_pr2_cam_intrinsics.sh
+++ pr2_calibration_launch/write_urdf/write_pr2_cam_intrinsics.sh
@@ -4,19 +4,20 @@
 if [ "$1" = ""]; then
     echo "Usage: ./write_cam_intrinsics [bagfile]"
 else
-    config=`rospack find pr2_calibration_launch`/results/system4.yaml
-
-    echo "Bagfile: [$1]"
+    # config=`rospack find pr2_calibration_launch`/results/system4.yaml
+    config=/tmp/pr2_calibration/system4.yaml
+    bagfile=/tmp/pr2_calibration/cal_measurements.bag
+    echo "Bagfile: [$bagfile]"
     echo "Config File: [$config]"
     echo "Flashing Wide Stereo Right"
-    rosrun pr2_calibration_propagation propagate_baseline.py camera:=wide_stereo/right $1 $config wide_right_rect
+    rosrun pr2_calibration_propagation propagate_baseline.py camera:=wide_stereo/right $bagfile $config wide_right_rect
     echo "**********"
     echo "Flashing Wide Stereo Left"
-    rosrun pr2_calibration_propagation propagate_baseline.py camera:=wide_stereo/left $1 $config wide_left_rect
+    rosrun pr2_calibration_propagation propagate_baseline.py camera:=wide_stereo/left $bagfile $config wide_left_rect
     echo "**********"
     echo "Flashing Narrow Stereo Right"
-    rosrun pr2_calibration_propagation propagate_baseline.py camera:=narrow_stereo/right $1 $config narrow_right_rect
+    rosrun pr2_calibration_propagation propagate_baseline.py camera:=narrow_stereo/right $bagfile $config narrow_right_rect
     echo "**********"
     echo "Flashing Narrow Stereo Left"
-    rosrun pr2_calibration_propagation propagate_baseline.py camera:=narrow_stereo/left $1 $config narrow_left_rect
+    rosrun pr2_calibration_propagation propagate_baseline.py camera:=narrow_stereo/left $bagfile $config narrow_left_rect
 fi
