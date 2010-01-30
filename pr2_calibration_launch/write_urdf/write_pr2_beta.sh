bc8de6916768dfbbef646826cedd3b393871d8a3
vpradeep <devnull@localhost>
2010-01-30 01:55:56 +0000

    Now always writing new URDF based on /etc/ros/urdf/robot.xml

--- pr2_calibration_launch/write_urdf/write_pr2_beta.sh
+++ pr2_calibration_launch/write_urdf/write_pr2_beta.sh
@@ -2,7 +2,7 @@
 
 original_system=`rospack find pr2_calibration_launch`/estimate_params/config_pr2_beta/system.yaml
 final_system=/tmp/pr2_calibration/system5.yaml
-urdf_in=/etc/ros/urdf/robot_uncalibrated.xml
+urdf_in=/etc/ros/urdf/robot.xml
 urdf_out=robot_calibrated.xml
 `rospack find pr2_calibration_propagation`/scripts/propagate_config_ref_positions.py $original_system $final_system $urdf_in $urdf_out
 echo "Wrote new URDF to $urdf_out"
