b03ff39cd37762b641cfcfd4592ce2005a33b8b0
vpradeep <devnull@localhost>
2010-03-02 23:08:56 +0000

    urdf and cam writers now point to system_calibrated.yaml

--- pr2_calibration_launch/write_urdf/write_pr2_beta.sh
+++ pr2_calibration_launch/write_urdf/write_pr2_beta.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 original_system=`rospack find pr2_calibration_launch`/estimate_params/config_pr2_beta/system.yaml
-final_system=/tmp/pr2_calibration/system5.yaml
+final_system=/tmp/pr2_calibration/system_calibrated.yaml
 urdf_in=/etc/ros/urdf/robot.xml
 urdf_out=robot_calibrated.xml
 `rospack find pr2_calibration_propagation`/scripts/propagate_config_ref_positions.py $original_system $final_system $urdf_in $urdf_out
