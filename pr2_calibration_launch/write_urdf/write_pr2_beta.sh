d0279ee465cf506d1f92e1c862bf6bac4eccaca1
vpradeep <devnull@localhost>
2010-04-21 19:06:58 +0000

    Upgraded to using the new bagfile urdf writer

--- pr2_calibration_launch/write_urdf/write_pr2_beta.sh
+++ pr2_calibration_launch/write_urdf/write_pr2_beta.sh
@@ -2,7 +2,13 @@
 
 original_system=`rospack find pr2_calibration_launch`/estimate_params/config_pr2_beta/system.yaml
 final_system=/tmp/pr2_calibration/system_calibrated.yaml
-urdf_in=/etc/ros/urdf/robot.xml
+bag_in=/tmp/pr2_calibration/cal_measurements.bag
 urdf_out=robot_calibrated.xml
-`rospack find pr2_calibration_propagation`/scripts/propagate_config_ref_positions.py $original_system $final_system $urdf_in $urdf_out > /tmp/pr2_calibration/urdf_writer_debug.log
-echo "Wrote new URDF to $urdf_out"
+`rospack find pr2_calibration_propagation`/scripts/propagate_config2.py $original_system $final_system $bag_in $urdf_out > /tmp/pr2_calibration/urdf_writer_debug.log
+if [ $? = 0 ]
+then
+    echo "Wrote new URDF to $urdf_out"
+else
+    echo "Error writing URDF"
+fi
+
