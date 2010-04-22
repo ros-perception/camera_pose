4d9e9e417078fbf607785895a67229ef31f96846
vpradeep <devnull@localhost>
2010-04-22 00:28:11 +0000

    Piping estimation output to file

--- pr2_calibration_launch/estimate_params/estimate_pr2_beta_urdf.sh
+++ pr2_calibration_launch/estimate_params/estimate_pr2_beta_urdf.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-roslaunch pr2_calibration_launch estimate_pr2_beta.launch
+roslaunch pr2_calibration_launch estimate_pr2_beta.launch | tee /tmp/pr2_calibration/calibration_estimation.log
 rosrun pr2_calibration_launch write_pr2_beta.sh
 rosrun pr2_calibration_launch backup_measurements.sh
