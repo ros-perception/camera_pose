1145e35d939eda273921d32e339f88800c0122c9
vpradeep <devnull@localhost>
2010-06-17 21:28:03 +0000

    Accidentally commented out too much on last commit

--- pr2_calibration_launch/estimate_params/estimate_pr2_beta_urdf.sh
+++ pr2_calibration_launch/estimate_params/estimate_pr2_beta_urdf.sh
@@ -26,5 +26,5 @@ if [ "$est_return_val" -ne "0" ]; then
 fi
 
 #roslaunch pr2_calibration_launch estimate_pr2_beta.launch | tee /tmp/pr2_calibration/calibration_estimation.log
-#rosrun pr2_calibration_launch write_pr2_beta.sh
-#rosrun pr2_calibration_launch backup_measurements.sh
+rosrun pr2_calibration_launch write_pr2_beta.sh
+rosrun pr2_calibration_launch backup_measurements.sh
