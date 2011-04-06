fc5b1a62577aedb137aa449154e24168cdfa223d
Stuart Glaser <sglaser@willowgarage.com>
2011-04-06 16:30:49 -0700

    Wrapped initial pose estimation into functions.

--- megacal_estimation/init_optimization_cb.py
+++ megacal_estimation/init_optimization_cb.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python
 
-BAG = '/u/vpradeep/kinect_bags/kinect_extrinsics_2011-04-05-16-01-28.bag'
-
 import sys, time, optparse
 import itertools
 import collections
@@ -19,26 +17,29 @@ def pose_msg_to_kdl(m):
                        PyKDL.Vector(m.position.x, m.position.y, m.position.z))
 
 
-# Stores the checkerboards observed by two cameras
-# camera_id -> camera_id -> [ (cb pose, cb pose, cb id) ]
-mutual_observations = collections.defaultdict(lambda: collections.defaultdict(list))
 
-bag = rosbag.Bag(BAG)
-checkerboard_id = 0
-for topic, msg, t in bag:
-    assert topic == 'robot_measurement'
-    for M_cam1, M_cam2 in itertools.combinations(msg.M_cam, 2):
-        cam1 = M_cam1.camera_id
-        cam2 = M_cam2.camera_id
-        p1 = pose_msg_to_kdl(M_cam1.features.object_pose.pose)
-        p2 = pose_msg_to_kdl(M_cam2.features.object_pose.pose)
-        
-        mutual_observations[cam1][cam2].append( (p1, p2, checkerboard_id) )
-        mutual_observations[cam2][cam1].append( (p2, p1, checkerboard_id) )
+def read_observations(bag_filename):
+    # Stores the checkerboards observed by two cameras
+    # camera_id -> camera_id -> [ (cb pose, cb pose, cb id) ]
+    mutual_observations = collections.defaultdict(lambda: collections.defaultdict(list))
 
-    checkerboard_id += 1
+    bag = rosbag.Bag(bag_filename)
+    checkerboard_id = 0
+    for topic, msg, t in bag:
+        assert topic == 'robot_measurement'
+        for M_cam1, M_cam2 in itertools.combinations(msg.M_cam, 2):
+            cam1 = M_cam1.camera_id
+            cam2 = M_cam2.camera_id
+            p1 = pose_msg_to_kdl(M_cam1.features.object_pose.pose)
+            p2 = pose_msg_to_kdl(M_cam2.features.object_pose.pose)
 
-bag.close()
+            mutual_observations[cam1][cam2].append( (p1, p2, checkerboard_id) )
+            mutual_observations[cam2][cam1].append( (p2, p1, checkerboard_id) )
+
+        checkerboard_id += 1
+
+    bag.close()
+    return mutual_observations
 
 # Populates cameras_seen and checkerboards_seen
 # root_cam: camera id to start from
@@ -63,12 +64,18 @@ def bfs(root_cam, observations, cameras_seen, checkerboards_seen):
             for cb in checkerboards:
                 if cb[2] not in checkerboards_seen:
                     checkerboards_seen[cb[2]] = cam1_pose * cb[0]
-    
 
-root_cam = mutual_observations.keys()[0]
-camera_poses = {}
-checkerboard_poses = {}
-bfs(root_cam, mutual_observations, camera_poses, checkerboard_poses)
+def find_initial_poses(bag_filename, root_cam = None):
+    mutual_observations = read_observations(bag_filename)
+    if not root_cam:
+        root_cam = mutual_observations.keys()[0]
+    camera_poses = {}
+    checkerboard_poses = {}
+    bfs(root_cam, mutual_observations, camera_poses, checkerboard_poses)
+    return camera_poses, checkerboard_poses
+
+BAG = '/u/vpradeep/kinect_bags/kinect_extrinsics_2011-04-05-16-01-28.bag'
+camera_poses, checkerboard_poses = find_initial_poses(BAG)
 
 print camera_poses
 print "\n\n"
