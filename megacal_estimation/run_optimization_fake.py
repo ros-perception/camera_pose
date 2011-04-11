fccdd6386aac915730379cde4478e66c9c7a9463
Vijay Pradeep <vpradeep@willowgarage.com>
2011-04-11 13:35:19 -0700

    Messing with fake opt

--- megacal_estimation/run_optimization_fake.py
+++ megacal_estimation/run_optimization_fake.py
@@ -22,7 +22,7 @@ from megacal_estimation import estimate
 
 # checkerboard points in checkerboard frame
 check_points = []
-for x in range(0, 2):
+for x in range(0, 4):
     for y in range(-1, 1):
         check_points.append(PyKDL.Vector(x, y, 1))
 
@@ -32,29 +32,29 @@ cal_estimate = CalibrationEstimate()
 
 camera_a = CameraPose()
 camera_a.camera_id = 'cam_a'
-camera_a.pose = posemath.toMsg(PyKDL.Frame(PyKDL.Rotation.RPY(0, pi/2.0, 0), PyKDL.Vector(0, 0 ,0)))
-#camera_a.pose = posemath.toMsg(PyKDL.Frame(PyKDL.Vector(0, 0 ,0)))
+#camera_a.pose = posemath.toMsg(PyKDL.Frame(PyKDL.Rotation.RPY(0, pi/2.0, 0), PyKDL.Vector(0, 0 ,0)))
+camera_a.pose = posemath.toMsg(PyKDL.Frame(PyKDL.Vector(0, 0 ,0)))
 
 camera_b = CameraPose()
 camera_b.camera_id = 'cam_b'
-camera_b.pose = posemath.toMsg(PyKDL.Frame(PyKDL.Rotation.RPY(0,pi/2.0,0), PyKDL.Vector(0, -1, 0)))
-#camera_b.pose = posemath.toMsg(PyKDL.Frame(PyKDL.Vector(0, -1, 0)))
+#camera_b.pose = posemath.toMsg(PyKDL.Frame(PyKDL.Rotation.RPY(0,pi/2.0,0), PyKDL.Vector(0, -1, 0)))
+camera_b.pose = posemath.toMsg(PyKDL.Frame(PyKDL.Vector(0, -1, 0)))
 
-target_1 = posemath.toMsg(PyKDL.Frame(PyKDL.Rotation.RPY(0, pi/7.0, 0), PyKDL.Vector(1, 0, 0)))
-#target_1 = posemath.toMsg(PyKDL.Frame(PyKDL.Vector(0, 0, 1)))
-target_2 = posemath.toMsg(PyKDL.Frame(PyKDL.Rotation.RPY(0, pi/3.0, 0), PyKDL.Vector(2, 0, 0)))
+#target_1 = posemath.toMsg(PyKDL.Frame(PyKDL.Rotation.RPY(0, pi/7.0, 0), PyKDL.Vector(1, 0, 0)))
+target_1 = posemath.toMsg(PyKDL.Frame(PyKDL.Vector(0, 0, 1)))
+#target_2 = posemath.toMsg(PyKDL.Frame(PyKDL.Rotation.RPY(0, pi/3.0, 0), PyKDL.Vector(2, 0, 0)))
 #target_2 = posemath.toMsg(PyKDL.Frame(PyKDL.Vector(0, 0, 1)))
 
 cal_estimate.cameras = [camera_a, camera_b]
-cal_estimate.targets = [target_1, target_2]
+cal_estimate.targets = [target_1]
 print cal_estimate
 
 
 # generate samples
 scale = 0.00
 offset = PyKDL.Vector(0, 0.1, 0)
-#P = [1,    0,   1,      0,     0,   1,     1,      0,     0, 0, 1, 0]
-P = [525,   0,   319.5,  0,     0,   525,   239.5,  0,     0, 0, 1,  0]
+P = [1,    0,   0,      0,     0,   1,     0,      0,     0, 0, 1, 0]
+#P = [525,   0,   319.5,  0,     0,   525,   239.5,  0,     0, 0, 1,  0]
 P_mat = reshape( matrix(P, float), (3,4) )
 
 cal_pattern = CalibrationPattern()
@@ -88,7 +88,19 @@ print cal_samples
 
 # add offset
 cal_estimate.cameras[0].pose = posemath.toMsg(posemath.fromMsg(cal_estimate.cameras[0].pose) * PyKDL.Frame(offset))
-
+#cal_estimate.cameras[1].pose = posemath.toMsg(posemath.fromMsg(cal_estimate.cameras[1].pose) * PyKDL.Frame(offset))
 
 # Run optimization
 estimate.enhance(cal_samples, cal_estimate)
+
+#residual, J = estimate.calculate_residual_and_jacobian(cal_samples, cal_estimate)
+
+#set_printoptions(linewidth=300, precision=5, suppress=True)
+#print "Jacobian:\n", J
+#print "Residual:\n", reshape(residual, [-1,2])
+
+#step = linalg.pinv(J)*residual
+#print "Unscaled Step:\n", reshape(step, [-1, 6]).T
+#print "RMS: ", estimate.rms(residual)
+#next_estimate = estimate.oplus(cal_estimate, step)
+#print "Next Estimate:\n", next_estimate
