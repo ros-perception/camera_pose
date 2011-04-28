c8fa18d7068feac102aef1f6750343096512f358
Wim Meeussen <wim@willowgarage.com>
2011-04-28 13:53:33 -0700

    make test work with tf1 so we don't have to wait for tf2 fixes and re-release

--- camera_pose_calibration/test/test_camera_pose_calibration.py
+++ camera_pose_calibration/test/test_camera_pose_calibration.py
@@ -3,8 +3,9 @@
 PKG = 'camera_pose_calibration'
 import roslib; roslib.load_manifest(PKG)
 import rospy
-import tf2_ros
-import tf2_kdl
+#import tf2_ros
+#import tf2_kdl
+import tf
 import PyKDL
 import unittest
 import rostest
@@ -12,22 +13,28 @@ import rostest
 
 class TestCameraPose(unittest.TestCase):
     def test_one(self):
-        tf_buffer = tf2_ros.Buffer()
-        tf_listener = tf2_ros.TransformListener(tf_buffer)
+        #tf_buffer = tf2_ros.Buffer()
+        #tf_listener = tf2_ros.TransformListener(tf_buffer)
+        tf_listener = tf.TransformListener()
+        fr1 = 'camera_a/openni_depth_optical_frame'
+        fr2 = 'camera_c/prosilica_optical_frame'
 
-        tr_check = PyKDL.Frame(PyKDL.Rotation.Quaternion(0.022, 0.007, -0.006, 1.000), PyKDL.Vector(-0.011, 0.027, 0.048))
-        tr_identity = tf2_ros.Stamped(PyKDL.Frame.Identity(), rospy.Time(0), 'camera_c/prosilica_optical_frame')
-        rospy.sleep(15)
-        tr_res = tf_buffer.transform(tr_identity, 'camera_a/openni_depth_optical_frame', rospy.Duration(60))
-        diff = PyKDL.diff(tr_check*tr_res.Inverse(), PyKDL.Frame.Identity())
+        #tr_check = PyKDL.Frame(PyKDL.Rotation.Quaternion(0.022, 0.007, -0.006, 1.000), PyKDL.Vector(-0.011, 0.027, 0.048))
+        #tr_identity = tf2_ros.Stamped(PyKDL.Frame.Identity(), rospy.Time(0), fr2)
+        #tr_res = tf_buffer.transform(tr_identity, fr1, rospy.Duration(60))
+        #diff = PyKDL.diff(tr_check*tr_res.Inverse(), PyKDL.Frame.Identity())
+        #self.assertAlmostEqual(diff.rot.Norm(), 0.0, 1)
+        #self.assertAlmostEqual(diff.vel.Norm(), 0.0, 1)
 
-        print "tr_check" , tr_check.p, tr_check.M.GetQuaternion()
-        print "tr_check_inv" , tr_check.Inverse().p, tr_check.Inverse().M.GetQuaternion()
-        print "tr_res" , tr_res.p, tr_res.M.GetQuaternion()
-        print "tr_res_inv" , tr_res.Inverse().p, tr_res.Inverse().M.GetQuaternion()
-        print "Diff " , diff
-        self.assertAlmostEqual(diff.rot.Norm(), 0.0, 1)
-        self.assertAlmostEqual(diff.vel.Norm(), 0.0, 1)
+        tf_listener.waitForTransform(fr1, fr2, rospy.Time(0), rospy.Duration(60))
+        ((x, y, z), (rx, ry, rz, rw)) = tf_listener.lookupTransform(fr1, fr2, rospy.Time(0))
+        self.assertAlmostEqual(x, -0.011, 1)
+        self.assertAlmostEqual(y, 0.027, 1)
+        self.assertAlmostEqual(z, 0.048, 1)
+        self.assertAlmostEqual(rx, 0.022, 1)
+        self.assertAlmostEqual(ry, 0.007, 1)
+        self.assertAlmostEqual(rz, -0.006, 1)
+        self.assertAlmostEqual(rw, 1.00, 1)
         
 
 
