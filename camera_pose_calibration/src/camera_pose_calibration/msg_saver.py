f85bbebc62245efbf066f0a7730939244983b8fa
Wim Meeussen <wim@willowgarage.com>
2011-04-28 14:28:02 -0700

    Adding arg for msg_saver filename

--- camera_pose_calibration/src/camera_pose_calibration/msg_saver.py
+++ camera_pose_calibration/src/camera_pose_calibration/msg_saver.py
@@ -12,16 +12,19 @@ import rostopic
 
 def callback(msg):
     global topic_name, bag_filename
-    bag = rosbag.Bag(bag_filename, 'w')
-    bag.write(topic_name, msg)
-    print "Saving msg on [%s]" % topic_name
-    bag.close()
+    if (bag_filename):
+        bag = rosbag.Bag(bag_filename, 'w')
+        bag.write(topic_name, msg)
+        print "Saving msg on [%s]" % topic_name
+        bag.close()
 
-if len(rospy.myargv()) >= 4:
+if len(rospy.myargv()) >= 3:
     msg_type = std_msgs.msg.Empty
     topic_name = rospy.myargv()[1]
     topic_type = rospy.myargv()[2]
-    bag_filename = rospy.myargv()[3]
+    bag_filename = None
+    if len(rospy.myargv()) >= 4:
+        bag_filename = rospy.myargv()[3]
 else:
     #print ""
     print "   Message Saver:"
@@ -49,19 +52,21 @@ rospy.init_node("msg_saver", disable_rosout=True, disable_rostime=True)
 pub = rospy.Publisher(topic_name, msg_class, latch=True)
 
 # Get the last message on the topic of interest
-if (os.path.isfile(bag_filename)):
-    print "Found file [%s]" % bag_filename
-    bag = rosbag.Bag(bag_filename)
-    last_msg = None
-    for topic, msg, t in bag.read_messages(topics=[topic_name]):
-        last_msg = msg
-    if last_msg is not None:
-        pub.publish(last_msg)
+
+if bag_filename:
+    if (os.path.isfile(bag_filename)):
+        print "Found file [%s]" % bag_filename
+        bag = rosbag.Bag(bag_filename)
+        last_msg = None
+        for topic, msg, t in bag.read_messages(topics=[topic_name]):
+            last_msg = msg
+        if last_msg is not None:
+            pub.publish(last_msg)
+        else:
+            print "Couldn't find any messages on topic [%s]" % topic_name
+        bag.close()
     else:
-        print "Couldn't find any messages on topic [%s]" % topic_name
-    bag.close()
-else:
-    print "Couldn't find file [%s]. Skipping publishing" % bag_filename
+        print "Couldn't find file [%s]. Skipping publishing" % bag_filename
 
 # Listen on the same topic, so that we can update the bag
 sub = rospy.Subscriber(topic_name, msg_class, callback)
