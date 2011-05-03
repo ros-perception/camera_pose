e35325ccd3c9c2d089f8c26d8c7f398753a9fedd
Vijay Pradeep <vpradeep@willowgarage.com>
2011-05-03 10:41:49 -0700

    Optimizer now handles binning and ROI

--- camera_pose_calibration/src/camera_pose_calibration/camera_info_converter.py
+++ camera_pose_calibration/src/camera_pose_calibration/camera_info_converter.py
@@ -1,11 +1,55 @@
 #!/usr/bin/python
 
+import copy
 import roslib; roslib.load_manifest('camera_pose_calibration')
 import rospy
 from sensor_msgs.msg import CameraInfo
 
 
+def unbin(msg_in):
+    '''
+    Modify projection matrix such that it corresponds to the binned image
+    '''
+    msg_out = copy.deepcopy(msg_in)
+    b_x = msg_in.binning_x
+    b_y = msg_in.binning_y
+    off_x = msg_in.roi.x_offset
+    off_y = msg_in.roi.y_offset
 
+    P = list(msg_in.P)
+
+    if b_x == 0:
+        b_x = 1
+    if b_y == 0:
+        b_y = 1
+
+    P[0] /= b_x
+    P[1] /= b_x
+    P[2] = (P[2] - off_x)/b_x
+    P[3] /= b_x
+
+    P[4] /= b_y
+    P[5] /= b_y
+    P[6] = (P[6] - off_y)/b_y
+    P[7] /= b_y
+
+    msg_out.P = P
+    msg_out.binning_x = 1
+    msg_out.binning_y = 1
+
+    msg_out.height /= b_x
+    msg_out.width /= b_y
+
+    msg_out.roi.x_offset = 0
+    msg_out.roi.y_offset = 0
+    msg_out.roi.height = msg_out.height
+    msg_out.roi.width = msg_out.width
+
+    msg_out.D = []
+    msg_out.K = (P[0], P[1], P[2],
+                 P[4], P[5], P[6],
+                 P[8], P[9], P[10])
+    return msg_out;
 
 
 class CameraInfoConverter:
@@ -13,46 +57,8 @@ class CameraInfoConverter:
         self.pub = rospy.Publisher('camera_info_out', CameraInfo)
         self.sub = rospy.Subscriber('camera_info_in', CameraInfo, self.cam_info_cb)
 
-
     def cam_info_cb(self, msg):
-        b_x = msg.binning_x
-        b_y = msg.binning_y
-        off_x = msg.roi.x_offset
-        off_y = msg.roi.y_offset
-
-        P = list(msg.P)
-
-        P[0] /= b_x
-        P[1] /= b_x
-        P[2] = (P[2] - off_x)/b_x
-        P[3] /= b_x
-
-        P[4] /= b_y
-        P[5] /= b_y
-        P[6] = (P[6] - off_y)/b_y
-        P[7] /= b_y
-
-        msg.P = P
-        msg.binning_x = 1
-        msg.binning_y = 1
-
-        msg.height /= b_x
-        msg.width /= b_y
-
-        msg.roi.x_offset = 0
-        msg.roi.y_offset = 0
-        msg.roi.height = msg.height
-        msg.roi.width = msg.width
-
-        msg.D = []
-        msg.K = (P[0], P[1], P[2],
-                 P[4], P[5], P[6],
-                 P[8], P[9], P[10])
-
-        self.pub.publish(msg)
-
-
-
+        self.pub.publish(unbin(msg))
 
 def main():
     rospy.init_node('camera_info_converter')
